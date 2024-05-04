ce790df452772da8bc3ca9a5c20ef95ec91258d2
Caua Stupp <cauastupp@gmail.com>
2024-05-04 15:53:56 -0300

    API Node com Postgres

---
+++ create-table.js
@@ -0,0 +1,16 @@
+import sql from './sql.js';
+
+// sql`DROP TABLE IF EXISTS videos;`.then(() => {
+//   console.log('table is deleted')
+// })
+
+sql`
+  CREATE TABLE videos (
+    id         TEXT PRIMARY KEY,
+    title      TEXT,
+    description TEXT,
+    duration    INTEGER
+  )
+`.then(() => {
+  console.log('table created')
+})
